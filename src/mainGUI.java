7703db801dd47b96fea98ebbae51078e04ea0cf0
DonFruendo <tobi_kessi@yahoo.de>
2016-03-31 22:06:05 +0200

    GUI init

---
+++ src/mainGUI.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * NeuralNets
+ * Created by Don on 31.03.2016.
+ */
+public class mainGUI extends JFrame {
+    private JPanel consolePanel;
+    private JPanel inputPanel;
+    private JPanel outputPanel;
+    private JPanel mainPanel;
+    private JSlider sliderLeft;
+    private JSlider sliderFrontLeft;
+    private JSlider sliderFront;
+    private JSlider sliderFrontRight;
+    private JSlider sliderRight;
+    private JTextArea console;
+    private JLabel outputLeft;
+    private JLabel outputFrontLeft;
+    private JLabel outputFront;
+    private JLabel outputFrontRight;
+    private JLabel outputRight;
+
+    public static void main(String[] args) {
+        mainGUI frame = new mainGUI("mainGUI");
+        frame.setContentPane(frame.mainPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public mainGUI(String caption) {
+        super(caption);
+    }
+
+    public void setValues(ArrayList<Integer> values) {
+        outputLeft.setText(values.get(0) + "");
+        outputFrontLeft.setText(values.get(1) + "");
+        outputFront.setText(values.get(2) + "");
+        outputFrontRight.setText(values.get(3) + "");
+        outputRight.setText(values.get(4) + "");
+    }
+
+    public void addToConsole(String line) {
+        console.setText(line + "\n" + console.getText());
+    }
+}
